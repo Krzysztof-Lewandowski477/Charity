db34dfb119ffbcd07ea707fb2bb099e096b5fe26
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-16 17:11:56 +0100

    HomePageController add Donation function in index

--- src/main/java/pl/coderslab/charity/controllers/HomePageController.java
+++ src/main/java/pl/coderslab/charity/controllers/HomePageController.java
@@ -1,16 +1,27 @@
 package pl.coderslab.charity.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.domain.repository.DonationRepository;
 
 @Controller
 @RequestMapping
 public class HomePageController {
+    private final DonationRepository donationRepository;
+
+    public HomePageController(DonationRepository donationRepository) {
+        this.donationRepository = donationRepository;
+    }
 
     @GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+
+        model.addAttribute ( "donations", donationRepository.findAll () );
 
         return "index";
     }
+
+
 }
