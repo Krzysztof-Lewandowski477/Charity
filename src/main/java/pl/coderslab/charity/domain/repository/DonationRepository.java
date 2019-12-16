c04c7299b24e78af30e46069d1464fa51b2d1498
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-16 18:31:37 +0100

    DonationRepository create query sumQuantity

--- src/main/java/pl/coderslab/charity/domain/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/domain/repository/DonationRepository.java
@@ -1,15 +1,13 @@
 package pl.coderslab.charity.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
+
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.charity.domain.entities.Donation;
 
 public interface DonationRepository extends JpaRepository<Donation,Long> {
 
-    @Transactional
-    @Modifying
-    @Query(value = "SELECT sum(quantity) FROM donations ")
-    Donation sumOfQuantity();
+    @Query(value = "SELECT sum(quantity) FROM Donation ")
+    public Long sumQuantities();
+
 }
