716a5f66b310eb9740b8930125670763e9a47df3
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-16 18:31:55 +0100

    InstitutionRepository

--- src/main/java/pl/coderslab/charity/domain/repository/InstitutionRepository.java
+++ src/main/java/pl/coderslab/charity/domain/repository/InstitutionRepository.java
@@ -1,4 +1,11 @@
 package pl.coderslab.charity.domain.repository;
 
-public interface InstitutionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.coderslab.charity.domain.entities.Institution;
+
+
+
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+
+
 }
