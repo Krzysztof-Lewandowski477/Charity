360e32ab42e7ae422bf94c7adc149152d5e22ff1
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-17 17:15:54 +0100

    Donation add method interface

--- src/main/java/pl/coderslab/charity/services/DonationServices.java
+++ src/main/java/pl/coderslab/charity/services/DonationServices.java
@@ -1,6 +1,10 @@
 package pl.coderslab.charity.services;
 
+import pl.coderslab.charity.dtos.DonationDataDTO;
+
 public interface DonationServices {
+
+     void addDonation(DonationDataDTO donationData);
 
      Long sumQuantitiesBags();
      Long institutionsSum();
