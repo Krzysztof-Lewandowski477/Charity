117176f1468d0fc82bf7f1ebd69dbc3cb129eace
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-17 16:13:34 +0100

    add slf4j and rqArgConstr.

--- src/main/java/pl/coderslab/charity/services/impl/DefaultDonationServices.java
+++ src/main/java/pl/coderslab/charity/services/impl/DefaultDonationServices.java
@@ -1,5 +1,7 @@
 package pl.coderslab.charity.services.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,7 +10,8 @@ import pl.coderslab.charity.services.DonationServices;
 
 
 @Transactional
-@Service
+@Service @Slf4j
+@RequiredArgsConstructor
 public class DefaultDonationServices implements DonationServices {
 
 
