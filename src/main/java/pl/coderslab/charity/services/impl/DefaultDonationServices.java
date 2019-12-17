a960284e05e073b1ca329eb33ae80651a48185c5
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-17 14:58:09 +0100

    InstitutionSum query Impl

--- src/main/java/pl/coderslab/charity/services/impl/DefaultDonationServices.java
+++ src/main/java/pl/coderslab/charity/services/impl/DefaultDonationServices.java
@@ -1,16 +1,28 @@
 package pl.coderslab.charity.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.charity.domain.repository.DonationRepository;
 import pl.coderslab.charity.services.DonationServices;
 
+
+@Transactional
+@Service
 public class DefaultDonationServices implements DonationServices {
+
 
     @Autowired
     private DonationRepository donationRepository;
 
+
     @Override
-    public Long sumQuantities() {
-        return donationRepository.sumQuantities ();
+    public Long sumQuantitiesBags() {
+        return donationRepository.sumQuantitiesBag ();
+    }
+
+    @Override
+    public Long institutionsSum() {
+        return donationRepository.institutionsSum ();
     }
 }
