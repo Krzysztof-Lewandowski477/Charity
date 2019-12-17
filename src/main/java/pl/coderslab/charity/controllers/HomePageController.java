eba14609fb6b99fdd55585c8124ae3152ea61d5c
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-17 16:12:57 +0100

    small changes in HomePageController

--- src/main/java/pl/coderslab/charity/controllers/HomePageController.java
+++ src/main/java/pl/coderslab/charity/controllers/HomePageController.java
@@ -4,22 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.charity.domain.repository.DonationRepository;
+import pl.coderslab.charity.domain.repository.InstitutionRepository;
+import pl.coderslab.charity.services.DonationServices;
+
 
 @Controller
 @RequestMapping
 public class HomePageController {
-    private final DonationRepository donationRepository;
+    private final InstitutionRepository institutionRepository;
+    private final DonationServices donationServices;
 
-    public HomePageController(DonationRepository donationRepository) {
-        this.donationRepository = donationRepository;
+
+    public HomePageController( InstitutionRepository institutionRepository, DonationServices donationServices) {
+        this.institutionRepository = institutionRepository;
+        this.donationServices = donationServices;
+
+
     }
 
     @GetMapping("/")
     public String getHomePage(Model model) {
 
-        model.addAttribute ( "donations", donationRepository.findAll () );
-
+        model.addAttribute ( "institutions", institutionRepository.findAll () );
+        model.addAttribute ( "quantityBags", donationServices.sumQuantitiesBags () );
+        model.addAttribute ( "organizations", donationServices.institutionsSum () );
         return "index";
     }
 
