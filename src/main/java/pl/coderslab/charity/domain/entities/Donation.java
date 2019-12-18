31da6ee949a49c8433d2d985d82c101756aad512
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-18 09:37:06 +0100

    Donation

--- src/main/java/pl/coderslab/charity/domain/entities/Donation.java
+++ src/main/java/pl/coderslab/charity/domain/entities/Donation.java
@@ -8,7 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,12 +33,12 @@ public class Donation {
     private String zipCode;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate pickUpDate;
-    private LocalDateTime pickUpTime;
+    private LocalTime pickUpTime;
     private String pickUpComment;
 
     @ManyToMany
     private List<Category> categories = new ArrayList<> ();
-
+//po darach sumowanie distinct
     @ManyToOne
     private Institution institution;
 }
