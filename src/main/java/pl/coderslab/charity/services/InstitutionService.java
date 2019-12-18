07524f1d7d7256a2c4b18312276f7a8346550916
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-18 09:30:52 +0100

    InstitutionService add save method

---
+++ src/main/java/pl/coderslab/charity/services/InstitutionService.java
@@ -0,0 +1,9 @@
+package pl.coderslab.charity.services;
+
+import pl.coderslab.charity.dtos.InstitutionDataDTO;
+
+public interface InstitutionService {
+
+    void save(InstitutionDataDTO institutionDataDTO);
+
+}
