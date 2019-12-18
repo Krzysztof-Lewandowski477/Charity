531a7f1a9ad30c0d0afcdfcd1da3e0643fc4c72b
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-18 09:38:04 +0100

    add id

--- src/main/java/pl/coderslab/charity/dtos/InstitutionDataDTO.java
+++ src/main/java/pl/coderslab/charity/dtos/InstitutionDataDTO.java
@@ -7,8 +7,11 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class InstitutionDataDTO {
 
+
+    private Long id;
     @NotBlank
     private String name;
     @NotBlank
     private String description;
+
 }
