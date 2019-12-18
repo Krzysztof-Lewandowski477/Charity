93ca1224bbfb80dd1bcd5d50e1c10f09a2b44983
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-18 09:30:32 +0100

    InstitutionService add save method

---
+++ src/main/java/pl/coderslab/charity/services/impl/DefaultInstitutionService.java
@@ -0,0 +1,29 @@
+package pl.coderslab.charity.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.coderslab.charity.domain.entities.Institution;
+import pl.coderslab.charity.domain.repository.InstitutionRepository;
+import pl.coderslab.charity.dtos.InstitutionDataDTO;
+import pl.coderslab.charity.services.InstitutionService;
+
+@Transactional
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class DefaultInstitutionService implements InstitutionService {
+
+    @Autowired
+    private InstitutionRepository institutionRepository;
+
+    @Override
+    public void save(InstitutionDataDTO institutionDataDTO) {
+        ModelMapper modelMapper = new ModelMapper ();
+        Institution institution = modelMapper.map(institutionDataDTO,Institution.class);
+        institutionRepository.save ( institution );
+    }
+}
