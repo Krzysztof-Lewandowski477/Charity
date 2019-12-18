7c811f97b29485021cc468f51b1c50b7cb7fc365
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-18 09:31:33 +0100

    add save method

--- src/main/java/pl/coderslab/charity/services/impl/DefaultCategoryService.java
+++ src/main/java/pl/coderslab/charity/services/impl/DefaultCategoryService.java
@@ -2,14 +2,26 @@ package pl.coderslab.charity.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.coderslab.charity.domain.entities.Category;
 import pl.coderslab.charity.domain.repository.CategoryRepository;
+import pl.coderslab.charity.dtos.CategoryDataDTO;
 import pl.coderslab.charity.services.CategoryService;
 @Transactional
 @Service @Slf4j @RequiredArgsConstructor
 public class DefaultCategoryService implements CategoryService {
 
-    private final CategoryRepository categoryRepository;
+    @Autowired
+     private CategoryRepository categoryRepository;
 
+
+    @Override
+    public void save(CategoryDataDTO categoryDataDTO) {
+        ModelMapper modelMapper = new ModelMapper ();
+        Category category = modelMapper.map ( categoryDataDTO,Category.class );
+        categoryRepository.save ( category );
+    }
 }
