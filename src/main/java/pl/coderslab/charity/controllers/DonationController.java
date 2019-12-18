8fdfa736e2ed01f73f3cb158e8de62a7125f3cbd
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-18 09:36:28 +0100

    DonationController add category

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.domain.repository.CategoryRepository;
 import pl.coderslab.charity.dtos.DonationDataDTO;
+import pl.coderslab.charity.services.DonationServices;
 
 import javax.validation.Valid;
 
@@ -15,21 +17,29 @@ import javax.validation.Valid;
 @RequestMapping
 public class DonationController {
 
+    private final CategoryRepository categoryRepository;
+    private final DonationServices donationServices;
+
+    public DonationController(CategoryRepository categoryRepository, DonationServices donationServices) {
+        this.categoryRepository = categoryRepository;
+        this.donationServices = donationServices;
+    }
+
     @GetMapping("/form")
     public String formGet(Model model){
 
-        model.addAttribute ( "donations", new DonationDataDTO () );
+        model.addAttribute ( "categories", categoryRepository.findAll () );
 
         return "form";
     }
 
     @PostMapping("/form")
-    public String formPost(@ModelAttribute("donations")@Valid DonationDataDTO donationData, BindingResult result){
+    public String formPost(@ModelAttribute("categories")@Valid DonationDataDTO donationData, BindingResult result){
 
         if(result.hasErrors ()){
             return "redirect:/form";
         }
-
+        donationServices.addDonation ( donationData );
         return "form-confirmation";
     }
 
