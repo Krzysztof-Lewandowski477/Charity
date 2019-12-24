4a67ff6dd0012231cd8b25e6a99a714195995af9
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-24 20:14:32 +0100

    Login only

---
+++ src/main/java/pl/coderslab/charity/controllers/LoginController.java
@@ -0,0 +1,38 @@
+package pl.coderslab.charity.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String prepareLoginPage(HttpServletRequest request){
+        if(request.isUserInRole ( "ROLE_USER" )){
+            return "index";
+        }else if(request.isUserInRole ( "ROLE_ADMIN" )){
+            return "admin";
+        }
+        return "login";
+    }
+
+    // nie da się GÓWNA ODPALIĆ DO ADMINA
+    @GetMapping("/logged")
+    public String onlyForLoggedIn(HttpServletRequest request) {
+        if (request.isUserInRole ( "ROLE_USER" )) {
+            return "logged";
+        }
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/admin")
+    public String onlyForLoggedAdmin(HttpServletRequest request) {
+        if (request.isUserInRole("ROLE_ADMIN")) {
+            return "admin";
+        }
+        return "redirect:/admin";
+    }
+}
