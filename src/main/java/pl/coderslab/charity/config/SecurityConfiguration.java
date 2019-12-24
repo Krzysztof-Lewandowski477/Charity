5eade7c15e630ed35c3e1f395450d7d8f670a19e
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-24 20:13:26 +0100

    Security fix

--- src/main/java/pl/coderslab/charity/config/SecurityConfiguration.java
+++ src/main/java/pl/coderslab/charity/config/SecurityConfiguration.java
@@ -30,23 +30,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication ()
-                .dataSource ( dataSource )
-                .passwordEncoder ( passwordEncoder () )
-                .usersByUsernameQuery ( "SELECT username, password, active FROM users WHERE username = ?" )
-                .authoritiesByUsernameQuery ( "SELECT u.username, r.name FROM users u JOIN users_roles ur ON u.id = ur.user_id JOIN roles r ON ur.roles_id = r.id WHERE u.username = ?" );
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT email, password, active FROM users WHERE email = ?")
+                .authoritiesByUsernameQuery("SELECT u.email, r.name FROM users u JOIN users_roles ur ON u.id" +
+                        "= ur.user_id JOIN roles r ON ur.roles_id = r.id WHERE u.email = ?");
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring ().antMatchers ( "/public_html/**" ,"/image/**").antMatchers ( "/webjars/**" )
+        web.ignoring ().antMatchers ( "/public_html/**" ,"/image/**" ,"/resources/**").antMatchers ( "/webjars/**" )
                 .antMatchers ( "/h2-console","/h2-console/**" );
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests ()
-                .antMatchers ( "/image/**","/**"  ).permitAll ()
+                .antMatchers ( "/image/**" ).permitAll ()
                 .antMatchers ( "/register" ).permitAll ()
                 .antMatchers ( "/login" ).permitAll ()
                 .antMatchers ( "/logout","/contacts").authenticated ()
@@ -56,9 +57,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and ()
                 .formLogin ()
                 .loginPage ( "/login")
-                .usernameParameter ( "username" )
+                .usernameParameter ( "email" )
                 .passwordParameter ( "password" )
-                .defaultSuccessUrl ( "/index" )
+                .defaultSuccessUrl ( "/" )
                 .and ()
                 .logout ()
                 .logoutUrl ( "/logout" )
