90ae510ec04e8456dc2aa78c5dca985285693e08
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-25 18:14:22 +0100

    AdminCOntroller
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/controllers/AdminController.java
+++ src/main/java/pl/coderslab/charity/controllers/AdminController.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.domain.entities.Category;
 import pl.coderslab.charity.domain.entities.Role;
 import pl.coderslab.charity.domain.entities.User;
+import pl.coderslab.charity.domain.repository.CategoryRepository;
 import pl.coderslab.charity.domain.repository.InstitutionRepository;
 import pl.coderslab.charity.domain.repository.RoleRepository;
 import pl.coderslab.charity.domain.repository.UserRepository;
@@ -19,14 +21,22 @@ public class AdminController {
     private final InstitutionRepository institutionRepository;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final CategoryRepository categoryRepository;
 
 
-    public AdminController(InstitutionRepository institutionRepository, UserRepository userRepository, RoleRepository roleRepository) {
+    public AdminController(InstitutionRepository institutionRepository, UserRepository userRepository, RoleRepository roleRepository, CategoryRepository categoryRepository) {
         this.institutionRepository = institutionRepository;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.categoryRepository = categoryRepository;
     }
-
+/*model categories vwievs*/
+    @ModelAttribute("catrgories")
+    public List<Category> categories () {
+        return categoryRepository.findAll (  );
+    }
+/*moddel implements in adnim.jsp
+* method findById(role)*/
     @ModelAttribute("admins")
     public List<User> admins() {
         Role role = roleRepository.findById(2);
