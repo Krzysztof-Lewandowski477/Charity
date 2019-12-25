4504b8a540d65a67b7609af1ebbb2f521ea35816
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2019-12-25 18:13:49 +0100

    UserRepository
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/domain/repository/UserRepository.java
+++ src/main/java/pl/coderslab/charity/domain/repository/UserRepository.java
@@ -1,7 +1,17 @@
 package pl.coderslab.charity.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.coderslab.charity.domain.entities.Role;
 import pl.coderslab.charity.domain.entities.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository  extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    List<User> findUsersByRoles(Role role);
 }
