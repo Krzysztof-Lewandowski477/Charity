38c5eb583451ebdb99e5be77c086688ba6ed9b59
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-05 17:29:48 +0100

    User
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/domain/entities/User.java
+++ src/main/java/pl/coderslab/charity/domain/entities/User.java
@@ -1,30 +1,44 @@
 package pl.coderslab.charity.domain.entities;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
 
 import javax.persistence.*;
-import java.util.List;
+import java.util.*;
 
 @Getter
 @Setter
 @ToString(exclude = {""})
 @EqualsAndHashCode(of = "id")
+@RequiredArgsConstructor
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userid;
+    private Long id;
     private String name;
     private String surname;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
-    private int active = 1;
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "userid"), inverseJoinColumns = @JoinColumn(name = "roleid"))
-    private List<Role> roles;
+    @Column(nullable = false)
+    @Transient
+    private String passwordConfirm;
+    private Boolean active = Boolean.FALSE;
+    @ManyToMany
+    private Set<Role> roles = new HashSet<> (  );
+    @OneToMany(mappedBy = "user")
+    private List<Donation> donations = new ArrayList<>();
+
+
+
+
+//    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+//    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "userid"), inverseJoinColumns = @JoinColumn(name = "roleid"))
+//    private List<Role> roles;
 }
