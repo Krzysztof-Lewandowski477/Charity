7afbc22b8982ee93a89490526140307a2881ae88
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-07 16:05:16 +0100

    compplete donation form finally!
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -1,22 +1,27 @@
 package pl.coderslab.charity.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.domain.entities.Category;
+import pl.coderslab.charity.domain.entities.Donation;
 import pl.coderslab.charity.domain.entities.Institution;
 import pl.coderslab.charity.domain.repository.CategoryRepository;
+import pl.coderslab.charity.domain.repository.DonationRepository;
 import pl.coderslab.charity.domain.repository.InstitutionRepository;
-import pl.coderslab.charity.dtos.DonationDataDTO;
+import pl.coderslab.charity.domain.repository.UserRepository;
 import pl.coderslab.charity.services.DonationServices;
 
-import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -27,40 +32,48 @@ public class DonationController {
     private final CategoryRepository categoryRepository;
     private final DonationServices donationServices;
     private final InstitutionRepository institutionRepository;
+    private final UserRepository userRepository;
+    private final DonationRepository donationRepository;
 
-    public DonationController(CategoryRepository categoryRepository, DonationServices donationServices, InstitutionRepository institutionRepository) {
+    public DonationController(CategoryRepository categoryRepository, DonationServices donationServices, InstitutionRepository institutionRepository, UserRepository userRepository, DonationRepository donationRepository) {
         this.categoryRepository = categoryRepository;
         this.donationServices = donationServices;
         this.institutionRepository = institutionRepository;
+        this.userRepository = userRepository;
+        this.donationRepository = donationRepository;
     }
 
-    @ModelAttribute("categories")
-    public List<Category> getCategories() {
-        return categoryRepository.findAll();
-    }
-
-//    @ModelAttribute("institutions")
-//    public List<Institution> getInstitutions() {
-//        return institutionRepository.findAll();
-//    }
 
     @GetMapping("/form")
-    public String formGet(Model model){
-        model.addAttribute ( "institutions", institutionRepository.findAll () );
-        model.addAttribute ( "donations" , new DonationDataDTO ());
-
-        return "donation" ;
+    public String prepareDonation(Model model){
+        model.addAttribute("institutions", institutionRepository.findAll());
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "form";
     }
 
-    @PostMapping(value = "/form")
-    public String formPost(@ModelAttribute("donations")@Valid DonationDataDTO donationData, BindingResult result){
-
-        if(result.hasErrors ()){
-            return "donation";
+    @PostMapping("/form")
+    public String processDonation(HttpServletRequest req, Model model, Principal principal){
+        Donation donation = new Donation();
+        List<Category> categories = new ArrayList<> ();
+        List<String> categoriesNames = Collections.singletonList(req.getParameter("categories"));
+        for(String category : categoriesNames){
+            categories.add(categoryRepository.findByName(category));
         }
-        log.debug ( "co sie stało {}", donationData );
-        donationServices.addDonation ( donationData );
-        log.debug ( "co sie stało :{}", donationData );
+        donation.setCategories(categories);
+        donation.setCity(req.getParameter("city"));
+        donation.setPickUpComment(req.getParameter("comment"));
+        donation.setPickUpDate( LocalDate.parse(req.getParameter("date")));
+        donation.setPickUpTime( LocalTime.parse(req.getParameter("time")));
+        donation.setUser(userRepository.findUserByEmail(principal.getName()));
+        donation.setQuantity(Integer.parseInt ( req.getParameter("bags")));
+        donation.setStreet(req.getParameter("street"));
+        donation.setZipCode(req.getParameter("zipCode"));
+        donation.setStatus("nieodebrany");
+        donation.setInstitution(institutionRepository.findByName(req.getParameter("organization")));
+        model.addAttribute("org", req.getParameter("organization"));
+        donationRepository.save(donation);
+
+
         return "form-confirmation";
     }
 
