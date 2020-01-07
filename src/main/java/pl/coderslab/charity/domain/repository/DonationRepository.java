cad1f3c19eff0c8990db0942a479893a27218ea9
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-07 21:34:11 +0100

    DonationRepository
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/domain/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/domain/repository/DonationRepository.java
@@ -2,12 +2,18 @@ package pl.coderslab.charity.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.charity.domain.entities.Donation;
+import pl.coderslab.charity.domain.entities.User;
 
 import java.util.List;
 
 public interface DonationRepository extends JpaRepository<Donation,Long> {
+
+    Donation findDonationById(Long id);
+
 
     @Query(value = "SELECT sum(quantity) FROM donations " , nativeQuery = true)
      Long sumQuantitiesBag();
@@ -17,5 +23,16 @@ public interface DonationRepository extends JpaRepository<Donation,Long> {
 
     @Query(value = "SELECT * FROM donations WHERE user_id = ? ORDER BY pick_up_date DESC", nativeQuery = true)
     List<Donation> findAllByUserId(Long id);
+    List<Donation>findAllByUserOrderByStatusDescPickUpDate(User user);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE donations SET status ='odebrany' WHERE id = ?1",nativeQuery = true)
+    void changeStatusToGet(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE donations SET status ='nieodebrany' WHERE id = ?1",nativeQuery = true)
+    void changeStatusToUnGet(Long id);
 
 }
