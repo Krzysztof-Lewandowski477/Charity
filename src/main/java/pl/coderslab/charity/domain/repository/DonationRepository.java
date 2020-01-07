b5571984670eeabb59d1b5f510719ffd22a5646b
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-07 14:53:54 +0100

    DonationRepository
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/domain/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/domain/repository/DonationRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.domain.entities.Donation;
 
+import java.util.List;
+
 public interface DonationRepository extends JpaRepository<Donation,Long> {
 
     @Query(value = "SELECT sum(quantity) FROM donations " , nativeQuery = true)
@@ -12,5 +14,8 @@ public interface DonationRepository extends JpaRepository<Donation,Long> {
 
     @Query(value = "SELECT count(DISTINCT institution_id) FROM donations", nativeQuery = true)
     Long institutionsSum();
+
+    @Query(value = "SELECT * FROM donations WHERE user_id = ? ORDER BY pick_up_date DESC", nativeQuery = true)
+    List<Donation> findAllByUserId(Long id);
 
 }
