ed7f27f6a7832cda2bfb91ef5a18558ab2f6aff9
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-07 14:54:25 +0100

    UserRepository
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/domain/repository/UserRepository.java
+++ src/main/java/pl/coderslab/charity/domain/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package pl.coderslab.charity.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.charity.domain.entities.Role;
 import pl.coderslab.charity.domain.entities.User;
 
@@ -9,9 +12,21 @@ import java.util.Optional;
 
 public interface UserRepository  extends JpaRepository<User, Long> {
 
-    Optional<User> findUserByEmail(String email);
 
-    boolean existsByEmail(String email);
-
+    User findUserById (Long id);
+    User findByName(String name);
+    User findUserByEmail(String email);
     List<User> findUsersByRoles(Role role);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE users SET email = ?1 WHERE id = ?2",nativeQuery = true)
+    void changeEmailUser(String email, Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE users SET password = ?2 WHERE email = ?1",nativeQuery = true)
+    void changePasswordByUsername(String email, String password);
+
 }
