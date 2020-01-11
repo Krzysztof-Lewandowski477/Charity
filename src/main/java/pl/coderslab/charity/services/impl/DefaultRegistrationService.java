f36ec0ad2cbc8f6e02d6949f40680af912a27775
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-11 13:53:01 +0100

    RegistrationChange
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/services/impl/DefaultRegistrationService.java
+++ src/main/java/pl/coderslab/charity/services/impl/DefaultRegistrationService.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.charity.domain.entities.Role;
 import pl.coderslab.charity.domain.entities.User;
+import pl.coderslab.charity.domain.entities.VerificationToken;
 import pl.coderslab.charity.domain.repository.RoleRepository;
+import pl.coderslab.charity.domain.repository.TokenRepository;
 import pl.coderslab.charity.domain.repository.UserRepository;
 import pl.coderslab.charity.dtos.RegistrationDataDTO;
+import pl.coderslab.charity.mail.EmailService;
 import pl.coderslab.charity.services.RegistrationService;
-
+@Transactional
 @Service
 @Slf4j
 public class DefaultRegistrationService implements RegistrationService {
@@ -19,23 +23,39 @@ public class DefaultRegistrationService implements RegistrationService {
         private final PasswordEncoder passwordEncoder;
         private final UserRepository userRepository;
         private final RoleRepository roleRepository;
+        private final ModelMapper mapper;
+        private final TokenRepository tokenRepository;
+        private final EmailService emailService;
 
-    public DefaultRegistrationService(PasswordEncoder passwordEncoder, UserRepository userRepository, RoleRepository roleRepository) {
+    public DefaultRegistrationService(PasswordEncoder passwordEncoder, UserRepository userRepository, RoleRepository roleRepository, ModelMapper mapper, TokenRepository tokenRepository, EmailService emailService) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.mapper = mapper;
+        this.tokenRepository = tokenRepository;
+        this.emailService = emailService;
     }
 
     @Override
     public void register(RegistrationDataDTO registrationDataDTO) {
-        ModelMapper modelMapper = new ModelMapper ();
-        User user = modelMapper.map(registrationDataDTO, User.class);
-        user.setActive ( Boolean.TRUE );
-        String encodedPassword = passwordEncoder.encode ( registrationDataDTO.getPassword () );
-        user.setPassword ( encodedPassword );
-        Role roleUser = roleRepository.getByName ( "ROLE_USER" );
-        user.getRoles ().add ( roleUser );
-        userRepository.save ( user );
+        log.debug("Registration data to create user: {}", registrationDataDTO);
+        User user = mapper.map(registrationDataDTO, User.class);
+        log.debug("User after mapping from registrationData: {}", user);
+        user.setActive(Boolean.FALSE);
+        String encodedPassword = passwordEncoder.encode(registrationDataDTO.getPassword());
+        user.setPassword(encodedPassword);
+        Role roleUser = roleRepository.getByName("ROLE_USER");
+        user.getRoles().add(roleUser);
+        VerificationToken verificationToken = new VerificationToken(user);
+        emailService.sendSimpleMessage(user.getEmail(), "Charity donation app: Complete your Registration!",
+                "To activate your account, please click here (link valid 24 hours) : "
+                        +"http://localhost:8080/confirm-account?token="+verificationToken.getToken());
+        log.debug("VerificationToken object: {}", verificationToken);
+        log.debug("User before save: {}", user);
+        userRepository.save(user);
+        log.debug("User after save: {}", user);
+        verificationToken.getUser().setId(userRepository.findUserByEmail(user.getEmail()).getId());
+        tokenRepository.save(verificationToken);
     }
 
 
