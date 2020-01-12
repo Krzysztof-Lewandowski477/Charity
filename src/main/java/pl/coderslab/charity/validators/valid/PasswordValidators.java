a209b8ecf61ffbf40a440d5f663778e541126902
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-12 21:08:21 +0100

    PasswordValidators
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

---
+++ src/main/java/pl/coderslab/charity/validators/valid/PasswordValidators.java
@@ -0,0 +1,41 @@
+package pl.coderslab.charity.validators.valid;
+
+import org.passay.*;
+import pl.coderslab.charity.validators.config.ValidPassword;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+public class PasswordValidators implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public void initialize(ValidPassword arg0) {
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        org.passay.PasswordValidator validator = new org.passay.PasswordValidator ( Arrays.asList (
+                new LengthRule ( 8, 25 ),
+                new CharacterRule ( EnglishCharacterData.UpperCase, 1 ),
+                new CharacterRule ( EnglishCharacterData.LowerCase, 1 ),
+                new CharacterRule ( EnglishCharacterData.Digit, 1 ),
+                new CharacterRule ( EnglishCharacterData.Special, 1 ),
+                new WhitespaceRule ()
+        ) );
+
+        RuleResult result = validator.validate ( new PasswordData ( password ) );
+        if (result.isValid ()) {
+            return true;
+        }
+
+        List<String> messages = validator.getMessages ( result );
+        String messageTemplate = String.join ( ", ", messages );
+
+        context.buildConstraintViolationWithTemplate ( messageTemplate )
+                .addConstraintViolation ()
+                .disableDefaultConstraintViolation ();
+        return false;
+    }
+}
