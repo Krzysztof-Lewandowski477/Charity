f370f37781d455a507269dda00adee911130af3f
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-13 17:18:48 +0100

    DonationServices
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/services/DonationServices.java
+++ src/main/java/pl/coderslab/charity/services/DonationServices.java
@@ -1,11 +1,14 @@
 package pl.coderslab.charity.services;
 
+import pl.coderslab.charity.dtos.DeliverStatusDTO;
 import pl.coderslab.charity.dtos.DonationDataDTO;
-
 public interface DonationServices {
 
      void addDonation(DonationDataDTO donationData);
 
      Long sumQuantitiesBags();
      Long institutionsSum();
+
+     void CourierVisit(DeliverStatusDTO statusDTO);
+
 }
