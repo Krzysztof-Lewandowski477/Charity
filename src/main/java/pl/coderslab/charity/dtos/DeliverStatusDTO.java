159cd95a8b8780f3219ed14478dbcd0160e041dd
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-13 17:19:07 +0100

    DeliverStatus
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

---
+++ src/main/java/pl/coderslab/charity/dtos/DeliverStatusDTO.java
@@ -0,0 +1,11 @@
+package pl.coderslab.charity.dtos;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class DeliverStatusDTO {
+    private Long id;
+    private String status;
+    private LocalDateTime deliverTime;
+}
