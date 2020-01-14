6253ced75fb8defbfc8c58c5dfd6d2b0c46b85fc
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-14 19:26:00 +0100

    PasswordDTO
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/dtos/PasswordDTO.java
+++ src/main/java/pl/coderslab/charity/dtos/PasswordDTO.java
@@ -1,10 +1,12 @@
 package pl.coderslab.charity.dtos;
 
 import lombok.Data;
+import pl.coderslab.charity.validators.config.SamePasswords;
+import pl.coderslab.charity.validators.config.ValidPassword;
 
-@Data
+@Data @SamePasswords
 public class PasswordDTO {
-
+    @ValidPassword
     private String password;
     private String rePassword;
 }
