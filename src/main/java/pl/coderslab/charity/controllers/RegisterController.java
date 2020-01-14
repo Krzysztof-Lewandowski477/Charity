76f0f55fd8c030f1ca14f9ff1db6ecd061ee6120
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-14 17:01:17 +0100

    active account by email complete
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/controllers/RegisterController.java
+++ src/main/java/pl/coderslab/charity/controllers/RegisterController.java
@@ -3,52 +3,60 @@ package pl.coderslab.charity.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import pl.coderslab.charity.dtos.RegistrationDataDTO;
 
 import pl.coderslab.charity.mail.EmailService;
 import pl.coderslab.charity.services.RegistrationService;
+import pl.coderslab.charity.services.TokenService;
 
 import javax.validation.Valid;
 
 
 @Controller
-@RequestMapping("/register")
 public class RegisterController {
 
     private final RegistrationService registrationService;
     private final EmailService mailService;
+    private final TokenService tokenService;
 
 
-
-    public RegisterController(RegistrationService registrationService, EmailService mailService) {
+    public RegisterController(RegistrationService registrationService, EmailService mailService, TokenService tokenService) {
         this.registrationService = registrationService;
-
         this.mailService = mailService;
+        this.tokenService = tokenService;
+    }
+
+    @RequestMapping("/confirm-account")
+    public String confirmRegistration(@RequestParam("token") String tokenUrl) {
+        if (tokenService.ActiveUser ( tokenUrl ).equals ( false )) {
+            return "index";
+        }
+        return "login";
     }
 
 
-    @GetMapping
+    @GetMapping("/register")
     public String getRegistrationPage(Model model) {
-        model.addAttribute("registrationData", new RegistrationDataDTO());
+        model.addAttribute ( "registrationData", new RegistrationDataDTO () );
         return "register";
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public String processRegistrationPage(@ModelAttribute("registrationData") @Valid RegistrationDataDTO registrationData, BindingResult results) {
-        if (results.hasErrors()) {
+        if (results.hasErrors ()) {
             return "register";
         }
-        registrationService.register(registrationData);
-        mailService.sendSimpleMessage(registrationData.getEmail(), "Register Confirmation", "test");
+        registrationService.register ( registrationData );
+
         return "redirect:/";
     }
 
 
-
 }
+
+
+
+
 
