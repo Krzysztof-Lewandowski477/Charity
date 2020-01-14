5ccb3bab8a1c0b192895d8ee368bc385b17dba7a
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-14 14:21:08 +0100

    CategoryService
    Signed-off-by: Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>

--- src/main/java/pl/coderslab/charity/services/CategoryService.java
+++ src/main/java/pl/coderslab/charity/services/CategoryService.java
@@ -2,11 +2,17 @@ package pl.coderslab.charity.services;
 
 import pl.coderslab.charity.dtos.CategoryDataDTO;
 
+import java.util.List;
+
 public interface CategoryService {
+
+    List<CategoryDataDTO> allCategory ();
 
     void save(CategoryDataDTO categoryDataDTO);
 
     void update(CategoryDataDTO categoryDataDTO ,Long id);
 
     void delete(CategoryDataDTO categoryDataDTO, Long id);
+
+
 }
